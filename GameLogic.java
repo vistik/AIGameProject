921e18464e7eae54481e1a6c744b6da8c600fa57
Visti Kløft <hr.kloft@gmail.com>
2012-03-12 22:35:02 +0100

    update

--- GameLogic.java
+++ GameLogic.java
@@ -1,9 +1,5 @@
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
+import java.util.*;
 
 // http://www.overclockers.com/forums/showthread.php?t=554653
 public class GameLogic implements IGameLogic {
@@ -12,8 +8,8 @@ public class GameLogic implements IGameLogic {
     private int y = 0;
     private int playerID;
     private int[][] board;
-    private int[] scale;
     private HashMap<String, Integer> memory;
+    private HashMap<String, Integer> patterns;
 
     public GameLogic() {
         // TODO Write your implementation for this method
@@ -25,32 +21,42 @@ public class GameLogic implements IGameLogic {
         this.playerID = playerID;
         this.board = new int[x][y];
         this.memory = new HashMap<String, Integer>();
-        this.scale = new int[5];
-        this.scale[0] = 0;
-        this.scale[1] = 1;
-        this.scale[2] = 3;
-        this.scale[3] = 9;
-        this.scale[4] = 1000;
-        
+        this.patterns = new HashMap<String, Integer>();;
+
+
+        this.patterns.put("1111", 1000);
+
+        this.patterns.put("1110", 9);
+        this.patterns.put("0111", 9);
+
+        this.patterns.put("1100", 3);
+        this.patterns.put("0110", 3);
+        this.patterns.put("0011", 3);
+
+        this.patterns.put("1000", 1);
+        this.patterns.put("0100", 1);
+        this.patterns.put("0010", 1);
+        this.patterns.put("0001", 1);
+
         // System.out.println(playerID);
         //
-        insertCoin(0, 1);
-        insertCoin(0, 1);
-        insertCoin(0, 1);
-        insertCoin(0, 1);
-
-
-        insertCoin(1, 1);
-        insertCoin(1, 1);
-        insertCoin(1, 1);
-        insertCoin(1, 1);
-
-        insertCoin(2, 1);
-        insertCoin(2, 1);
-        insertCoin(2, 1);
-
-
-        insertCoin(3, 1);
+//        insertCoin(0, 1);
+//        insertCoin(0, 1);
+//        insertCoin(0, 1);
+//        insertCoin(0, 2);
+//
+//
+//        insertCoin(1, 2);
+//        insertCoin(1, 2);
+//        insertCoin(1, 1);
+//        insertCoin(1, 1);
+//
+//        insertCoin(2, 1);
+//        insertCoin(2, 2);
+//        insertCoin(2, 2);
+//
+//
+//        insertCoin(3, 2);
         //
         // insertCoin(this.minmaxdecision(-1,2), 2);
         // // insertCoin(this.minmaxdecision(-1,1), 1);
@@ -61,19 +67,46 @@ public class GameLogic implements IGameLogic {
         // //
         // System.exit(0);
         // );
-        this.printBoard(board);
-        System.out.println("this.heuristicHorizontal(1, this.board)");
-        this.heuristicHorizontal(1, this.board);
-        
+//        this.printBoard(board);
+//        System.out.println("this.heuristicHorizontal(1, this.board)");
+//        System.out.println(this.heuristicHorizontal(1, this.board));
+//
 //        System.out.println("this.heuristicHorizontal(2, this.board)");
-//        this.heuristicHorizontal(2, this.board);
-        
-        System.out.println("this.heuristicVertical(1, this.board)");
-        this.heuristicVertical(1, this.board);
-        
+//        System.out.println(this.heuristicHorizontal(2, this.board));
+////
+//        System.out.println("this.heuristicVertical(1, this.board)");
+//        System.out.println(this.heuristicVertical(1, this.board));
+//
 //        System.out.println("this.heuristicVertical(2, this.board)");
-//        this.heuristicVertical(2, this.board);
-        System.exit(0);
+//        System.out.println(this.heuristicVertical(2, this.board));
+
+//        System.out.println(this.heuristics(1, this.board));
+//        System.out.println("Unsort Map......");
+//        Map<String, Integer> unsortMap = new HashMap<String, Integer>();
+//        unsortMap.put("1", 1);
+//        unsortMap.put("2", 44);
+//        unsortMap.put("3", 2);
+//        unsortMap.put("4", 1);
+//        unsortMap.put("5", 188);
+//        unsortMap.put("6", 6);
+//        unsortMap.put("7", 4);
+//        unsortMap.put("8", 2);
+//
+//        Iterator iterator = unsortMap.entrySet().iterator();
+//
+//        for (Map.Entry entry : unsortMap.entrySet()) {
+//            System.out.println("Key : " + entry.getKey()
+//                    + " Value : " + entry.getValue());
+//        }
+//
+//        System.out.println("Sorted Map......");
+//        Map<String, String> sortedMap = sortByComparator(unsortMap);
+//
+//        for (Map.Entry entry : sortedMap.entrySet()) {
+//            System.out.println("Key : " + entry.getKey()
+//                    + " Value : " + entry.getValue());
+//        }
+//        System.exit(0);
     }
 
     public String getHash(int[][] state) {
@@ -164,27 +197,95 @@ public class GameLogic implements IGameLogic {
     }
 
     public int decideNextMove() {
+        System.out.println("Memory:" + this.memory.size());
+        int oldmem = this.memory.size();
+//        this.memory = new HashMap<String, Integer>();
         System.out.println("Starts on finding move");
         Stopwatch st = new Stopwatch();
         st.start();
-        int nextMov = this.minmaxdecision(-1, this.playerID);
+        int nextMov = this.minmaxdecision(6, this.playerID);
         st.stop();
-        System.out.println(st.getElapsedTimeSecs());
-
+        System.out.println("secs: " + st.getElapsedTimeSecs());
+        System.out.println("Memory:" + this.memory.size());
+        System.out.println("New Memory:" + (this.memory.size() - oldmem));
+        
         // System.out.println();
         return nextMov;
     }
 
+    private static Map sortByComparator(Map unsortMap) {
+
+        List list = new LinkedList(unsortMap.entrySet());
+
+        //sort list based on comparator
+        Collections.sort(list, new Comparator() {
+
+            public int compare(Object o2, Object o1) {
+                return ((Comparable) ((Map.Entry) (o1)).getValue()).compareTo(((Map.Entry) (o2)).getValue());
+            }
+        });
+
+        //put sorted list into map again
+        Map sortedMap = new LinkedHashMap();
+        for (Iterator it = list.iterator(); it.hasNext();) {
+            Map.Entry entry = (Map.Entry) it.next();
+            sortedMap.put(entry.getKey(), entry.getValue());
+        }
+        return sortedMap;
+    }
+
     public ArrayList<int[][]> getPossibleActions(int playerID, int[][] state) {
         ArrayList<int[][]> actions = new ArrayList<int[][]>();
+//        Map<int[][], Integer> actions = new HashMap<int[][], Integer>();
+        ArrayList<int[][]> niceactions = new ArrayList<int[][]>();
         int[][] newState = null;
+        int hiscore = 0;
+        if (playerID == 2) {
+            hiscore = Integer.MIN_VALUE;
+        } else {
+            hiscore = Integer.MAX_VALUE;
+        }
+
         for (int i = 0; i < this.x; i++) {
             int[][] s = this.copyDoblAr(state);
             newState = insertCoin(i, playerID, s);
-
+            int h = this.heuristics(playerID, newState);
+//            actions.put(newState, h);
             actions.add(newState);
+//            if (playerID == 2) {
+//                if (h > hiscore) {
+//                    hiscore = h;
+//                }
+//            } else {
+//                if (h < hiscore) {
+//                    hiscore = h;
+//                }
+//            }
+
 
         }
+//        Map<int[][], Integer> sortedMap = sortByComparator(actions);
+//        int max = this.x - 2;
+//        int i = 0;
+//        for (Map.Entry entry : sortedMap.entrySet()) {
+//            if (max < i){
+//                break;
+//            }
+//            System.out.println("Key : " + entry.getKey()
+//                    + " Value : " + entry.getValue());
+//            niceactions.add((int[][])entry.getKey());
+//            i++;
+//        }
+
+//        for (int[][] action : actions) {
+//            int h = this.heuristics(playerID, action);
+//            if (h == hiscore) {
+//                niceactions.add(action);
+//            }
+//
+//        }
+
+//        System.out.println("nice:" + actions.size());
         return actions;
     }
 
@@ -227,14 +328,14 @@ public class GameLogic implements IGameLogic {
             }
             i++;
         }
-        System.out.println(primI);
-        System.out.println("ngoeg");
-        this.printBoard(mAction);
+//        System.out.println(primI);
+//        System.out.println("ngoeg");
+//        this.printBoard(mAction);
         return primI;
     }
 
     public int maxValue(int[][] state, int level, int max, int min) {
-        System.out.println(memory.size());
+//        System.out.println(memory.size());
         try {
             int v = ((Integer) memory.get(this.getHash(state))).intValue();
             return v;
@@ -245,15 +346,15 @@ public class GameLogic implements IGameLogic {
             case TIE:
                 return 0;
             case PLAYER1:
-                return 1;
+                return 1000;
             case PLAYER2:
-                return -1;
+                return -1000;
         }
-//		if (level == 0) {
-//			return 0;
-//		} else {
-//			level--;
-//		}
+        if (level == 0) {
+            return this.heuristics(1, state);
+        } else {
+            level--;
+        }
         mValue = Integer.MIN_VALUE;
         ArrayList<int[][]> actions = null;
         actions = this.getPossibleActions(1, state);
@@ -294,15 +395,15 @@ public class GameLogic implements IGameLogic {
             case TIE:
                 return 0;
             case PLAYER1:
-                return 1;
+                return 1000;
             case PLAYER2:
-                return -1;
+                return -1000;
         }
-//		if (level == 0) {
-//			return 0;
-//		} else {
-//			level--;
-//		}
+        if (level == 0) {
+            return this.heuristics(2, state);
+        } else {
+            level--;
+        }
 
         mValue = Integer.MAX_VALUE;
 
@@ -331,72 +432,87 @@ public class GameLogic implements IGameLogic {
     }
 
     public int heuristicHorizontal(int playerID, int[][] state) {
+        String hash = "";
+        int value = 0;
+        for (int j = 0; j < this.y; j++) {
+            hash = "";
+            for (int i = 0; i < this.x; i++) {
+                hash = hash + "" + state[i][j] + "";
+            }
+
+            Iterator it = this.patterns.entrySet().iterator();
+            while (it.hasNext()) {
+                Map.Entry pairs = (Map.Entry) it.next();
+
+                String key = (String) pairs.getKey();
+                key = key.replace("1", "" + playerID + "");
+                if (hash.indexOf(key) >= 0) {
+//                    System.out.println(key);
+                    value += (Integer) pairs.getValue();
+                }
+            }
+        }
         int factor;
-        if (playerID == 2){
+        if (playerID == 2) {
             factor = -1;
-        }else{
+        } else {
             factor = 1;
         }
-        int streak = 0;
-        int[] streaks = new int[4];
-        for (int j = 0; j < this.y; j++) {
-            for (int i = 0; i < this.x; i++) {
-                System.out.print(state[i][j]+ " ");
-                if (state[i][j] == playerID) {
-                    streak++;
-                } else {
-                    streaks[streak]++;
-                    streak = 0;
-                }
+        return value * factor;
 
-            }
-            System.out.print("\n");
+    }
+
+    public int heuristics(int playerID, int[][] state) {
+        if (state == null) {
+            return 0;
         }
+        int opponent = 0;
+        if (playerID == 1) {
+            opponent = 2;
+        } else {
+            opponent = 1;
+        }
+        int us = this.heuristicVertical(playerID, state) + this.heuristicHorizontal(playerID, state);
+        int them = this.heuristicVertical(opponent, state) + this.heuristicHorizontal(opponent, state);
+        return us + them;
+    }
+
+    public int heuristicVertical(int playerID, int[][] state) {
+
+        String hash = "";
         int value = 0;
-        int a = 0;
-        for (int i : streaks) {
-            System.out.println(i);
-            value = value + (i * this.scale[a]);
-            a++;
+        for (int j = 0; j < this.x; j++) {
+            hash = "";
+            for (int i = 0; i < this.y; i++) {
+                hash = hash + "" + state[j][i] + "";
+            }
+
+            Iterator it = this.patterns.entrySet().iterator();
+            while (it.hasNext()) {
+                Map.Entry pairs = (Map.Entry) it.next();
+
+                String key = (String) pairs.getKey();
+                key = key.replace("1", "" + playerID + "");
+                if (hash.indexOf(key) >= 0) {
+//                    System.out.println(key);
+                    value += (Integer) pairs.getValue();
+                }
+            }
         }
-        value = value * factor;
-        System.out.println(value);
-        return value;
+        int factor;
+        if (playerID == 2) {
+            factor = -1;
+        } else {
+            factor = 1;
+        }
+
+        return value * factor;
     }
     
-        public int heuristicVertical(int playerID, int[][] state) {
-        int factor;
-        if (playerID == 2){
-            factor = -1;
-        }else{
-            factor = 1;
+    public void heuristicsSouthEast(int playerID, int[][] state){
+        for(int y = this.y; y >= 0; y--){
+            
         }
-        int streak = 0;
-        int[] streaks = new int[4];
-        for (int j = 0; j < this.y; j++) {
-            for (int i = 0; i < this.x; i++) {
-                System.out.print(state[i][j] + " ");
-                if (state[j][i] == playerID) {
-                    streak++;
-                } else {
-                    streaks[streak]++;
-                    streak = 0;
-                }
-
-            }
-            System.out.print("\n");
-        }
-        System.out.println(streaks);
-        int value = 0;
-        int a = 0;
-        for (int i : streaks) {
-            System.out.println(i);
-            value = value + (i * this.scale[a]);
-            a++;
-        }
-        value = value * factor;
-        System.out.println(value);
-        return value;
     }
 
     public void printBoard(int[][] state) {
---
+++ NewClass.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author visti
+ */
+public class NewClass {
+    
+    
+    
+    
+    1 2 0 0
+    1 1 1 0
+    1 1 2 0
+    1 1 2 2
+    
+    1 0 0 0
+    0 1 0 0
+    0 0 1 0
+    0 0 0 1
+    1 1 0 0
+    0 1 1 0
+    0 0 1 1
+    1 1 1 0
+    0 1 1 1
+    1 1 1 1 
+}
